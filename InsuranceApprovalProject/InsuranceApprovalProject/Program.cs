3c319bff12247a24bc632ed00261c4ceac0ebee0
Chester-R <chesterrimando96@gmail.com>
2020-11-11 15:47:05 -0500

    Insurance Approval Project

---
+++ InsuranceApprovalProject/InsuranceApprovalProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceApprovalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insurance Approval");
+            Console.Write("What is your age? ");
+            string yourage= Console.ReadLine();
+            int age = Convert.ToInt32(yourage);
+
+            Console.Write("Have you ever had a DUI? [true/false] ");
+            string ans = Console.ReadLine();
+            bool ansDUI = Convert.ToBoolean(ans);
+
+            Console.Write("How many speeding tickets do you have? ");
+            string speedticket = Console.ReadLine();
+            int ticket = Convert.ToInt32(speedticket);
+
+            bool qualified = age > 15 && ansDUI == false && ticket <= 3;
+
+            Console.WriteLine("Qualified? " + qualified);
+            Console.ReadLine();
+
+
+        }
+    }
+}
