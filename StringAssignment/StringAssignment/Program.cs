6924a5c3bece2526439f1aeba24ee12b275106a3
Chester-R <chesterrimando96@gmail.com>
2020-11-17 14:39:46 -0500

    String Assignment

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Concatenate string
+            //string Greeting = "Good day, ";
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
+            //string message = ", hope you enjoy studying C#.";
+
+            //Console.WriteLine(Greeting + name + message);
+            //Console.ReadLine();
+
+            //2. String converted to upper case
+            //string Greeting = "Good day, ";
+            //Console.WriteLine("What is your name?");
+            //string name = Console.ReadLine();
+            //name = name.ToUpper();
+            //string message = ", hope you enjoy studying C#.";
+
+            //Console.WriteLine(Greeting + name + message);
+            //Console.ReadLine();
+
+            //3. String builder
+            StringBuilder sb1 = new StringBuilder("Quote:\n");
+            sb1.Append("\"I rather trust and regret,\n");
+            sb1.Append("than doubt and regret.\"\n");
+            sb1.Append("\t\t-Kirigaya Kazuto");
+            Console.WriteLine(sb1);
+            Console.ReadLine();
+
+        }
+    }
+}
