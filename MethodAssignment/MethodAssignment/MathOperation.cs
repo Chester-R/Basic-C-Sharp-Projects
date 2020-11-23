956eea7b9080de40bf0efc8816396b841f6a51eb
Chester-R <chesterrimando96@gmail.com>
2020-11-23 16:40:12 -0500

    Method Assignment

---
+++ MethodAssignment/MethodAssignment/MathOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class MathOperation
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;        
+        }
+        public static int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public static int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        
+        
+
+
+    }//End of MathOperation Class
+
+
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mathOperation = { "Addition", "Subtraction", "Multiplication", "Division"};
+            
+            //Input the two numbers to be used
+            Console.Write("Input 1st number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input 2nd number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Options of Math Operation
+            for (int i = 0; i < mathOperation.Length; i++)
+            {
+                Console.WriteLine($"[{i}] : {mathOperation[i]}");
+            }
+            Console.Write("Choose Math Operation to use: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            
+            //Math Operations
+            if (choice == 0)
+            {
+                int sum = MathOperation.Add(num1, num2);
+                Console.WriteLine($"{num1} + {num2} = {sum}");
+            }
+            else if (choice == 1)
+            {
+                int difference = MathOperation.Subtraction(num1, num2);
+                Console.WriteLine($"{num1} - {num2} = {difference}");
+            }
+            else if (choice == 2)
+            {
+                int product = MathOperation.Multiplication(num1, num2);
+                Console.WriteLine($"{num1} * {num2} = {product}");
+            }
+            else if (choice == 3)
+            {
+                int quotient = MathOperation.Division(num1, num2);
+                Console.WriteLine($"{num1} / {num2} = {quotient}");
+            }
+            else
+            {
+                Console.WriteLine("Wrong input for Math Operation.");
+            }
+
+            Console.ReadLine();
+        }
+
+    }//End of Program Class
+
+}
