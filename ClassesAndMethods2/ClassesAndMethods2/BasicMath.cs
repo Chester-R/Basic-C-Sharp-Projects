199c2ee87f2c931d13392e69e3e3bde6b8495f4f
Chester-R <chesterrimando96@gmail.com>
2020-11-24 17:22:35 -0500

    Classes and methods 2 assignment

---
+++ ClassesAndMethods2/ClassesAndMethods2/BasicMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods2
+{
+    class BasicMath
+    {
+
+        public static int MathOp(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+        public static int MathOp(int num1)
+        {
+            return num1 / 1 ;
+        }
+    }
+}
---
+++ ClassesAndMethods2/ClassesAndMethods2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input seconds number (not required): ");
+            string num = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(num))
+            {
+                int quotient = BasicMath.MathOp(num1);
+                Console.WriteLine($"Answer is equal to {quotient}");
+               
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(num);
+                int quotient = BasicMath.MathOp(num1, num2);
+                Console.WriteLine($"Answer is equal to {quotient}");
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
