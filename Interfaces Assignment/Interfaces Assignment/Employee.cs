9d8c41c68bfa053db12a46d072bd5f87d9ba3b19
Chester-R <chesterrimando96@gmail.com>
2020-11-26 15:13:28 -0500

    Interfaces assignment

---
+++ Interfaces Assignment/Interfaces Assignment/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_Assignment
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I quit.");
+        }
+
+        public int Id { get; set; }
+
+        
+
+    }
+}
