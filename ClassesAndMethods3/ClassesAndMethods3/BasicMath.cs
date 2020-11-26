a1e4c39a0eb2c1d9c7d0010c701713d7e04ab86e
Chester-R <chesterrimando96@gmail.com>
2020-11-25 21:45:39 -0500

    Classes and Methods Assignment 3

---
+++ ClassesAndMethods3/ClassesAndMethods3/BasicMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods3
+{
+    class BasicMath
+    {
+        public static void spaceDimension(int sqWidth, int height)
+        {
+            BasicMath dimension = new BasicMath();
+            dimension.SquareWidth = sqWidth;
+            dimension.Height = height;
+            int area = sqWidth * sqWidth;
+
+            Console.WriteLine($"Area = {area}");
+            Console.WriteLine($"Height = {dimension.Height}");
+        }
+
+        public int SquareWidth { get; set; }
+        public int Height { get; set; }
+    }
+}
---
+++ ClassesAndMethods3/ClassesAndMethods3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicMath.spaceDimension(sqWidth: 10, height: 5);
+
+            Console.ReadLine();
+        }
+    }
+}
