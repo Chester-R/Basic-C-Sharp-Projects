a0d70cdde7109cba1ab252efc69ec82fc9860bfa
Chester-R <chesterrimando96@gmail.com>
2020-11-26 00:00:42 -0500

    ClassMethodAssignment

---
+++ ClassMethodAssignment/ClassMethodAssignment/BasicMath.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class BasicMath
+    {
+        public void Divide(int num)
+        {
+            int quotient = num / 2;
+            Console.WriteLine($"{num} divide by 2 is equal to {quotient}");
+        }
+
+        public static void Divide(out int quotient, int num)
+        {
+            quotient = num / 2;
+        }
+
+      
+
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicMath math = new BasicMath();
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //method 1
+            //math.Divide(num);
+            //end of method 1
+
+            //method 2
+            int quotient = 0;
+            BasicMath.Divide(out quotient, num);
+            //end of method 2
+
+            //for method 2
+            Console.WriteLine($"{num} divide by 2 is equal to {quotient}");
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
