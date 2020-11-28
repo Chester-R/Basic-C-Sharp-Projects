131498de23533014092ce4fb1d9d5159c427cea5
Chester-R <chesterrimando96@gmail.com>
2020-11-27 22:15:37 -0500

    Update operator overload assignment

---
+++ OperatorOverloadingAssignment/OperatorOverloadingAssignment/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            employee1.setId(31);
+            employee2.setId(31);
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("Employee 1 is the same person as Employee 2.");
+            }
+            else if (employee1 != employee2)
+            {
+                Console.WriteLine("Employee 1 is the different person from Employee 2.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Employee
+    {
+        public int Id;
+
+        public void setId(int id)
+        {
+            Id = id;
+        }
+        
+        public static Employee operator+ (Employee a, Employee b)
+        {
+            Employee employee = new Employee();
+            employee.Id = a.Id;
+            employee.Id = b.Id;
+            return employee;
+        }
+
+        public static bool operator== (Employee emp1, Employee emp2)
+        {
+            bool status = false;
+            if (emp1.Id == emp2.Id)
+            {
+                status = true;
+            }
+            return status;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            bool status = false;
+            if (emp1.Id != emp2.Id)
+            {
+                status = true;
+            }
+            return status;
+        }
+
+    }
+}
