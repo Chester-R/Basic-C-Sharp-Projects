5ca9c718f0276d3422848f722791b3a6d450e412
Chester-R <chesterrimando96@gmail.com>
2020-11-28 10:57:56 -0500

    Generic Assignment

---
+++ GenericAssignment/GenericAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> Name = new Employee<string>();
+            Name.Things = new List<string>() { "Micheal", "Thomas", "Venti", "Keaya", "Diluc"};
+
+            Employee<int> Id = new Employee<int>() ;
+            Id.Things = new List<int>();
+            int id = 1000;
+
+            foreach (string name in Name.Things)
+            {
+                id++;
+                Id.Things.Add(id);
+                Console.WriteLine($"ID: {id} Name: {name}");
+                 
+            }
+            Console.ReadLine();
+        }
+    }
+}
