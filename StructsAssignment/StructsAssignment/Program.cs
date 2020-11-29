733c644ca42ba2f669b8a15cde53a53d85b9566b
Chester-R <chesterrimando96@gmail.com>
2020-11-29 14:19:23 -0500

    Struct Assignment

---
+++ StructsAssignment/StructsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignment
+{
+    class Program
+    {
+        public struct Number
+        {
+            public decimal Amount;
+        }
+        static void Main(string[] args)
+        {
+            Number amount1 = new Number();
+            amount1.Amount = 100.75m;
+
+            Console.WriteLine($"${amount1.Amount}");
+
+            Console.ReadLine();
+        }
+    }
+}
