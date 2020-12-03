bccfc2ce81db6d922e852aeeb64dd8031295cc72
Chester-R <chesterrimando96@gmail.com>
2020-12-03 13:56:11 -0500

    File I/O assignment

---
+++ FileIOAssignment/FileIOAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your phone number: ");
+            string phoneNumber = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\chest\source\repos\FileIOAssignment\logs.txt", phoneNumber);
+            string textPhoneNumber = File.ReadAllText(@"C:\Users\chest\source\repos\FileIOAssignment\logs.txt");
+            Console.WriteLine( $"Phone Number: { textPhoneNumber}" );
+
+            Console.ReadLine();
+        }
+    }
+}
