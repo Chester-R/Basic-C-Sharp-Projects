903f4c0b90745f19d792aed9df9a316baf3bbac2
Chester-R <chesterrimando96@gmail.com>
2020-12-05 09:40:44 -0500

    AdditionalFeatures

---
+++ AdditionalFeaturesAssignment/AdditionalFeaturesAssignment/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    class Employee
+    {
+        public Employee(string fName, string lName, int empId) : this(fName, lName, empId, "IT Department")
+        { 
+        
+        }
+        public Employee(string fName, string lName, int empId, string empDepartment)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Id = empId;
+            Department = empDepartment;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ AdditionalFeaturesAssignment/AdditionalFeaturesAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Chester";
+            var myAge = 24;
+
+            Console.WriteLine($"My name is {myName}, I am {myAge} year old.");
+
+            var employee = new Employee(myName, "Rimando", 1003);
+
+            Console.WriteLine($"Employee Name: {employee.FirstName} {employee.LastName} \nEmployee ID: {employee.Id} \n Employee Department: {employee.Department}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
