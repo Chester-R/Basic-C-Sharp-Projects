720e6a9e9f921878fdb74f04d71d0299e0997ec8
Chester-R <chesterrimando96@gmail.com>
2020-12-05 11:36:34 -0500

    Exception Handling part 2

---
+++ ExceptionHandlingAssignmentPart2/ExceptionHandlingAssignmentPart2/NegativeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingAssignmentPart2
+{
+    public class NegativeZerosException: Exception
+    {
+        public NegativeZerosException()
+            : base() { }
+        public NegativeZerosException(string message)
+            : base(message) { }
+    }
+}
---
+++ ExceptionHandlingAssignmentPart2/ExceptionHandlingAssignmentPart2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingAssignmentPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                var today = new DateTime();
+                today = DateTime.Now;
+                bool validAnswer = false;
+                int userAge = 0;
+                while (!validAnswer)
+                {
+                    Console.Write("User, enter your age: ");
+                    validAnswer = int.TryParse(Console.ReadLine(), out userAge);
+                    if (!validAnswer)
+                    {
+                        Console.WriteLine("Please enter digits only, no decimal.");
+                    }
+                    if (userAge <= 0)
+                    {
+                        throw new NegativeZerosException();
+                    }
+                }
+                var birthYear = today.Year - userAge;
+                Console.WriteLine($"Your birthyear is {birthYear}");
+
+            }
+            catch (NegativeZerosException)
+            {
+                Console.WriteLine("Invalid age format.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("System Error, please contact your System Administrator");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
